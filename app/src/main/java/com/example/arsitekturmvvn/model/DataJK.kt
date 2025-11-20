5d2ff7457b1fdb6edf886ae6e2184e4094037ee8
verss21 <arjunafrizal01@gmail.com>
2025-11-20 15:31:36 +0700

    Menambahkan Object

---
+++ app/src/main/java/com/example/arsitekturmvvn/model/DataJK.kt
@@ -0,0 +1,10 @@
+package com.example.arsitekturmvvn.model
+
+import com.example.arsitekturmvvn.R
+
+object DataJK {
+    val JenisK = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
