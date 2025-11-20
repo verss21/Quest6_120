a8cfd864c43dbbe451e94357ff4ee2fbf814d630
verss21 <arjunafrizal01@gmail.com>
2025-11-20 16:42:01 +0700

    Menambahkan value dan variable

--- app/src/main/java/com/example/arsitekturmvvn/view/FormSiswa.kt
+++ app/src/main/java/com/example/arsitekturmvvn/view/FormSiswa.kt
@@ -41,4 +41,8 @@ fun FormSiswa(
     pilihanJK: List<String>,
     onSubmitButtonClicked: (MutableList<String>) -> Unit,
     modifier: Modifier = Modifier
-)
+){
+    var txtNama by rememberSaveable { mutableStateOf("") }
+    var txtAlamat by remember { mutableStateOf("") }
+    var txtGender by remember { mutableStateOf("") }
+    val listDate: MutableList<String> = mutableListOf(txtNama, txtGender, txtAlamat)
